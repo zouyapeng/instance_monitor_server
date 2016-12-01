439353d61c574f574f745fb9350ad69ad60952e7
zouyapeng <zyp19901009@163.com>
2016-12-01 15:03:46 +0800

    添加uwsgi.ini 修改setting中sql的设置

--- instance_monitor_server/settings.py
+++ instance_monitor_server/settings.py
@@ -81,13 +81,23 @@ WSGI_APPLICATION = 'instance_monitor_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'instance_monitor',                      # Or path to database file if using sqlite3.
+        'USER': 'monitor',
+        'PASSWORD': 'monitor',
+        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '3306',                      # Set to empty string for default.
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
